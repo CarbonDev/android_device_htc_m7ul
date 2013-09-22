ff97f17148524aec0ebb210ff47717ecd9e1f9ff
Daniel MM <daxx4182@gmail.com>
2013-09-22 20:04:45 +0800

    Update identification to JWR66Y

--- carbon.mk
+++ carbon.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66Y.H1 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66Y.H1/239564:user/release-keys" PRIVATE_BUILD_DESC="3.07.1700.1 CL239564 release-keys"
 
 PRODUCT_NAME := carbon_m7ul
 PRODUCT_DEVICE := m7ul
