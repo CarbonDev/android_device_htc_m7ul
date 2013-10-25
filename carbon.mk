8d5420ec2353bdeb9b730dc00f37024f505a8139
Daniel MM <daxx4182@gmail.com>
2013-10-25 20:00:42 +0800

    Update identification
    Change-Id: I2baa78dff353a1b25f15ad42888b4108213438f6

--- carbon.mk
+++ carbon.mk
@@ -1,11 +1,20 @@
+# name
+PRODUCT_RELEASE_NAME := One
+
+# device
 $(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
+# phone
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66Y.H1 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66Y.H1/239564:user/release-keys" PRIVATE_BUILD_DESC="3.07.1700.1 CL239564 release-keys"
-
+# products
 PRODUCT_NAME := carbon_m7ul
+PRODUCT_BRAND := htc
 PRODUCT_DEVICE := m7ul
+PRODUCT_MODEL := One
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.m7ul.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=htc_europe \
+    PRIVATE_BUILD_DESC="3.57.401.500 CL259545 release-keys" \
+    BUILD_FINGERPRINT="htc/htc_europe/m7:4.3/JSS15J/259545.500:user/release-keys"
